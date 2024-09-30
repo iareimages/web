c301de2e3f0e9ae8fc318862088de653a3a20969
webpageiareimages <webimages.iare@gmail.com>
2024-10-01 00:06:05 +0530

    Update favcgushfjj.js

--- favcgushfjj.js
+++ favcgushfjj.js
@@ -1,4 +1,4 @@
-function showSelectedValues() {
+async function showSelectedValues() {
     const menu1 = document.getElementById('menu1').value;
     const menu2 = document.getElementById('menu2').value;
     const menu3 = document.getElementById('menu3').value;
